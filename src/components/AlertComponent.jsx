e333c836d0bb2fc64dfa4a9c29b4aab00a47338b
Akshy18 <jessiakshy36@gmail.com>
2025-04-24 17:03:45 +0530

    updated button in alert component

--- src/components/AlertComponent.jsx
+++ src/components/AlertComponent.jsx
@@ -12,16 +12,16 @@ const AlertComponent = ({ onCancel, header, text , activity }) => {
             {text}
         
         </p>
-        <p className="text-gray-700 text-center mb-6">
+        <p className="text-gray-700 text-center mb-6 font-medium">
           {activity[0].toUpperCase() + activity.slice(1)}
         </p>
 
         <div className="flex flex-col sm:flex-row justify-center gap-4">
           <button
             onClick={()=>onCancel(header)}
-            className="bg-red-700 hover:bg-red-600 text-white py-2 px-6 rounded-lg transition-all duration-200 font-medium flex-1 sm:flex-none"
+            className={`${header === 'Reminder !!!' ? 'bg-red-700 hover:bg-red-600 ' : 'bg-green-700 hover:bg-green-600 '} text-white py-2 px-6 rounded-lg transition-all duration-200 font-medium flex-1 sm:flex-none`}
           >
-            Cancel
+           {header === 'Reminder !!!' ? 'Cancel' : 'Ok'}
           </button>
         </div>
       </div>
